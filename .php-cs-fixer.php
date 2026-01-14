3a6c8a4e55067f51348051aa1c9c00a14b04f61b
Jay Klehr <jay@diablomedia.com>
2026-01-14 13:26:26 -0700

    php 8.5 (#342)
    * php 8.5
    
    * dep update

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,13 +9,15 @@ $finder = PhpCsFixer\Finder::create()
 $config = new PhpCsFixer\Config();
 return $config->setRiskyAllowed(true)
     ->setRules(array(
-        '@PSR2'                     => true,
-        '@PHPUnit60Migration:risky' => true,
-        'binary_operator_spaces'    => array('operators' => array('=' => 'align', '=>' => 'align')),
-        'single_quote'              => true,
-        'array_syntax'              => array('syntax' => 'long'),
-        'concat_space'              => array('spacing' => 'one'),
-        'psr_autoloading'           => array('dir' => 'src'),
+        '@PSR2'                          => true,
+        '@PHPUnit6x0Migration:risky'     => true,
+        'binary_operator_spaces'         => array('operators' => array('=' => 'align', '=>' => 'align')),
+        'single_quote'                   => true,
+        'array_syntax'                   => array('syntax' => 'long'),
+        'concat_space'                   => array('spacing' => 'one'),
+        'psr_autoloading'                => array('dir' => 'src'),
+        'switch_case_semicolon_to_colon' => true,
+        'short_scalar_cast'              => true,
     ))
     ->setUsingCache(true)
     ->setFinder($finder);
